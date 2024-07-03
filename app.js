7e7d4b06161185389b77a9926e1253436b2d2c37
Ashfaith <105349273+Ashfaith@users.noreply.github.com>
2024-07-03 19:47:49 +1200

    Added code to finish the game and refresh the page to start again. Added confettit celebration if player wins

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const result = document.querySelector(".result");
 const score = document.querySelector(".score");
+const playAgainBtn = document.getElementById('play-again-btn');
 
 // Generates computer choice
 const compBtn = document.querySelector(".compBtn");
@@ -54,7 +55,7 @@ let computerScore = 0;
 
 // Tests the choices against each other
 const compare = document.querySelector(".compare");
-const playAgain = document.querySelector(".play-again");
+const nextRound = document.querySelector(".next-round");
 
 const resultDisplay = document.createElement("div");
 compare.addEventListener('click', function() {
@@ -86,18 +87,82 @@ compare.addEventListener('click', function() {
     score.appendChild(scoreUpdate);
 
     swapButtons();
+    checkGameEnd();
 });
 
 //toggle play-again button
 function swapButtons() {
     compare.classList.toggle('hide');
-    playAgain.classList.toggle('hide');
+    nextRound.classList.toggle('hide');
 }
 
 // Clear the game text when clicked
-playAgain.addEventListener('click', function() {
+nextRound.addEventListener('click', function() {
     result.removeChild(resultDisplay);
     choiceDisplay.removeChild(humanDisplay);
     computerDisplay.removeChild(cmpDisplay);
     swapButtons();
 });
+
+playAgainBtn.addEventListener('click', function() {
+    window.location.reload()
+})
+
+let winner;
+function checkGameEnd() {
+    if (humanScore === 5 || computerScore === 5) {
+        winner = humanScore === 5 ? 'human' : 'computer';
+        resultDisplay.textContent = `${winner.charAt(0).toUpperCase() + winner.slice(1)} wins!`;
+        result.append(resultDisplay);
+
+        // Disable the buttons to prevent further gameplay
+        compBtn.disabled = true;
+        btnChoice.forEach(button => button.disabled = true);
+        compare.disabled = true;
+
+        // Show play again button
+        playAgainBtn.classList.remove('hide');
+        nextRound.classList.toggle('hide');
+        
+    }
+    if (winner === 'human'){
+        humanWin();
+    }
+}
+
+
+function humanWin() {
+    const defaults = {
+        spread: 360,
+        ticks: 100,
+        gravity: 0,
+        decay: 0.94,
+        startVelocity: 30,
+      };
+      
+      function shoot() {
+        confetti({
+          ...defaults,
+          particleCount: 30,
+          scalar: 1.2,
+          shapes: ["circle", "square"],
+          colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"],
+        });
+      
+        confetti({
+          ...defaults,
+          particleCount: 20,
+          scalar: 2,
+          shapes: ["emoji"],
+          shapeOptions: {
+            emoji: {
+              value: ["🦄", "🌈"],
+            },
+          },
+        });
+      }
+      
+      setTimeout(shoot, 0);
+      setTimeout(shoot, 100);
+      setTimeout(shoot, 200);
+}
